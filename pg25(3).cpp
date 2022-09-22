77fc76a44325f1e80864e2a62e60c7fdc8b96cf2
Guru charan <88940490+guru2k3@users.noreply.github.com>
2022-09-22 14:07:04 +0530

    Create pg25(3).cpp
    Reverse is :
    
    yadhtribyppah
    --------------------------------
    Process exited after 0.01924 seconds with return value 0
    Press any key to continue . . .

---
+++ pg25(3).cpp
@@ -0,0 +1,23 @@
+ #include <stdio.h>
+int main()
+{
+FILE *fp;
+char ch;
+int i,pos;
+fp=fopen("test.txt","r");
+if(fp==NULL)
+{
+printf("file doesnot exist \n");
+}
+fseek(fp,0,SEEK_END);
+pos=ftell(fp);
+i=0;
+printf("Reverse is :");
+while(i<pos)
+{
+i++;
+fseek(fp,-i,SEEK_END);
+ch=fgetc(fp);
+printf("%c",ch);
+}
+}
